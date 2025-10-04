d0897c12ec9a43a9707d9893b25c97ee42ab4389
hoondeveloper <hoonskyn9@gmail.com>
2025-10-04 14:27:46 +0900

    fix

--- server/scripts/rollup.config.mjs
+++ server/scripts/rollup.config.mjs
@@ -7,8 +7,8 @@ import typescript from "@rollup/plugin-typescript";
 export default {
   input: "src/index.ts",
   output: {
-    file: "dist/index.cjs",
-    format: "cjs",
+    file: "dist/index.js",
+    format: "esm",
   },
   plugins: [
     clean({
